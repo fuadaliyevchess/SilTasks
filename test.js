9d02f28e9cd40fa982726927e4a32a8e074e3309
fuadaliyevchess <fuadaliyevchess03@gmail.com>
2023-07-29 00:37:30 +0400

    modified

--- test.js
+++ test.js
@@ -14,16 +14,16 @@
 //1) AD soyad ata adinizi butov sheklde goturun ve array sheklinde herflerine ayirin
   let fullName = 'Fuad Aliyev Aliaga';
   let nameArray = fullName.split('');
-  console.log(nameArray);
+  // console.log(nameArray);
 
 //2) Herflere ayrilmish arrayda soyadinizi adinizin onune keciren alqoritm yazin
 
 let [sliceDad,sliceSurname,sliceName] = [nameArray.slice(12),nameArray.slice(5,12),nameArray.slice(0,5)];
 let concatFullName = sliceSurname.concat(sliceName,sliceDad)
-console.log(concatFullName)
+// console.log(concatFullName)
 //3) Alinan yeni arrayi stringe cevirin ama ilk array deyishmesin
 let strings = concatFullName.join('')
-console.log(strings)
+// console.log(strings)
 let arr = [4,5,2,1,5,2,5,3,5,6,2,1,6,2,6,2,5,3,2,7,4,6,4,5,6,2,5,6,3,7,3,7]
 
 // QEYD - Bu tasklarda "arr" arrayindan istifade edilecekdir.....
@@ -61,12 +61,12 @@ let remainderTwoIndex = arr.findIndex((num) => num % 3 === 2);
 
 //11) arraydaki en boyuk reqemin ilk indexini tapin
 let maxNumberIndex = arr.indexOf(Math.max(...arr));
-  console.log('Index of Max Number:', maxNumberIndex);
+  // console.log('Index of Max Number:', maxNumberIndex);
 
 //12) 4 reqeminin arrayin hansi indexlerinde oldugunu gosterin
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] == 4) {
-    console.log(i)
+    // console.log(i)
   }
 }
 //13) 5 reqeminin arraydaki en boyuk ve en kicik indexlerini tapin
@@ -83,11 +83,11 @@ for(var i=0; i<fiveArr.length;i++){
     enboyuk=fiveArr[i]
    }
 }
-console.log(enboyuk)
+// console.log(enboyuk)
 //14) "arr" - arrayindan reqemleri 2 den boyuk olan yeni array yaradin ve alinmish arrayla "arr" arrayinin uzunluqlari ferqini hesablayin
 let numbersGreaterThanTwo = arr.filter((num) => num > 2);
 let lengthDifference = numbersGreaterThanTwo.length - arr.length;
-console.log('Numbers greater than 2:', numbersGreaterThanTwo, 'Length difference:', lengthDifference);
+// console.log('Numbers greater than 2:', numbersGreaterThanTwo, 'Length difference:', lengthDifference);
 
 
 //15) 7 reqeminin indexleri cemini tapin.
@@ -95,7 +95,7 @@ let sumOfSevenIndexes = arr.reduce((sum, num, index) => {
   if (num === 7) sum += index;
   return sum;
 }, 0);
-console.log('Sum of 7 indexes:', sumOfSevenIndexes);
+// console.log('Sum of 7 indexes:', sumOfSevenIndexes);
 
 
 /////////////////////////////////////////////////////
@@ -158,57 +158,57 @@ let arr2 = [
 //QEYD Bu tasklarda arr2 istifade edilecekdir
 // 16 arr2 de "name"-i "t" herfi ile bashlayan obyektleri yeni arraya yigin
 let namesStartingWithT = arr2.filter((obj) => obj.name.startsWith('t'));
-console.log('Names starting with "t":', namesStartingWithT);
+// console.log('Names starting with "t":', namesStartingWithT);
 
 // 17 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin sayini tapin
 let countTStartingAndEnding = arr2.filter((obj) => obj.name.startsWith('t') && obj.name.endsWith('t')).length;
-console.log('Count of names starting and ending with "t":', countTStartingAndEnding);
+// console.log('Count of names starting and ending with "t":', countTStartingAndEnding);
 
 // 18 arr2 de "name"-i "t" herfi ile bashlayib "t" herfi ile biten obyektlerin "key"- lerinin cemini tapin
 let sumKeysTStartingAndEnding = arr2.reduce((sum, obj) => {
   if (obj.name.startsWith('t') && obj.name.endsWith('t')) sum += obj.key;
   return sum;
 }, 0);
-console.log('Sum of keys of names starting and ending with "t":', sumKeysTStartingAndEnding);
+// console.log('Sum of keys of names starting and ending with "t":', sumKeysTStartingAndEnding);
 
 // 19 arr2 de "name"-i "e" herfi ile biten obyeklerdeki name-in deyerini "SuperDev" sozu ile evezleyin.
 arr2.forEach((obj) => {
   if (obj.name.endsWith('e')) obj.name = 'SuperDev';
 });
-console.log('arr2 after replacing "e" ending names:', arr2);
+// console.log('arr2 after replacing "e" ending names:', arr2);
 
 // 20 arr2 de "name"-i en uzun olan obyektin key-i ni tapin
 let longestNameLength = Math.max(...arr2.map((obj) => obj.name.length));
 let keyOfLongestName = arr2.find((obj) => obj.name.length === longestNameLength).key;
-console.log('Key of the longest name:', keyOfLongestName);
+// console.log('Key of the longest name:', keyOfLongestName);
 
 // 21 arr2 de "name"-i en uzun olan obyektin indexin kvadratini hesablayin
 let indexOfLongestName = arr2.findIndex((obj) => obj.name.length === longestNameLength);
 let squareIndexOfLongestName = indexOfLongestName ** 2;
-console.log('Square of index of the longest name:', squareIndexOfLongestName);
+// console.log('Square of index of the longest name:', squareIndexOfLongestName);
 
 // 22 arr2 de "name"-inin uzunlugu 4 olan obyektlerden ibaret yeni array yaradin.
 let namesWithLengthFour = arr2.filter((obj) => obj.name.length === 4);
-console.log('Names with length 4:', namesWithLengthFour);
+// console.log('Names with length 4:', namesWithLengthFour);
 
 // 23 arr2 de en boyuk "key" - i olan obyektin "name"-i ni tapin
 let maxKey = Math.max(...arr2.map((obj) => obj.key));
 let nameOfMaxKey = arr2.find((obj) => obj.key === maxKey).name;
-console.log('Name of the max key:', nameOfMaxKey);
+// console.log('Name of the max key:', nameOfMaxKey);
 
 // 24 arr2 de terkibinde 2 'L' herfi olan obyekt(ler)in index(ler)ini tapin.
 let doubleLIndexes = arr2.reduce((indexes, obj, index) => {
   if (obj.name.includes('ll')) indexes.push(index);
   return indexes;
 }, []);
-console.log('Indexes of objects with "ll" in their names:', doubleLIndexes);
+// console.log('Indexes of objects with "ll" in their names:', doubleLIndexes);
 
 // 25 arr2 de terkibinde 2 't' herfi olan obyekt(ler)in key(ler)ini tapin.
 let doubleTKeys = arr2.reduce((keys, obj) => {
   if (obj.name.split('t').length - 1 === 2) keys.push(obj.key);
   return keys;
 }, []);
-console.log('Keys of objects with exactly 2 "t" letters in their names:', doubleTKeys);
+// console.log('Keys of objects with exactly 2 "t" letters in their names:', doubleTKeys);
 
 // 26 Bura da ozunuz arr2 ile ede bileceyiniz en maragli yeni funksionalligi yazin.
 // For example, let's find all unique keys in arr2 and display them in ascending order:
@@ -216,7 +216,7 @@ let uniqueKeys = arr2.reduce((keys, obj) => {
   if (!keys.includes(obj.key)) keys.push(obj.key);
   return keys;
 }, []).sort((a, b) => a - b);
-console.log('Unique keys in ascending order:', uniqueKeys);
+// console.log('Unique keys in ascending order:', uniqueKeys);
 
 
 // 27 Xaliqenin taski :D
